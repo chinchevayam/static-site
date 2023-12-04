f0acbfe60f79ae5bec35b6a3d61eee10ebedd1b6
Maria <chinchevaya.maria@gmail.com>
2023-12-04 20:30:13 +0000

    first commit

---
+++ .eleventy.js
@@ -0,0 +1,11 @@
+export default function(eleventyConfig) {
+    // Set custom directories for input, output, includes, and data
+    return {
+      dir: {
+        input: "src",
+        includes: "_includes",
+        data: "_data",
+        output: "_site"
+      }
+    };
+   };
